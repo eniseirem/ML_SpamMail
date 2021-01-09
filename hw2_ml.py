f4e82042b81f9614c5d2bdffadd91504e546fd9a
eniseirem <eniseirem@gmail.com>
2021-01-09 17:11:43 +0300

    adding plots

--- hw2_ml.py
+++ hw2_ml.py
@@ -11,9 +11,14 @@ data = pd.read_csv('data/dataset.txt', na_values=missing_values, header=None)
 #print(data.tail)
 
 from sklearn.neighbors import KNeighborsClassifier
-
+cm=[]
+recall = 0
+precision = 0
 #use 5 nearest neighbor (k = 5) and Euclidean distance to implement k-NN classifier.
 def KNN(X_train, y_train, X_valid, y_valid):
+    global recall
+    global precision
+    global knn
     knn = KNeighborsClassifier(n_neighbors=5,metric='euclidean')
     knn.fit(X_train,y_train)
     result = knn.predict(X_valid)
@@ -23,11 +28,14 @@ def KNN(X_train, y_train, X_valid, y_valid):
     cm = confusion_matrix(y_valid,result)
     print("Confusion matrix")
     print(cm)
+    from sklearn.metrics import recall_score
+    recall = recall_score(y_valid, result)
+    from sklearn.metrics import precision_score
+    precision = precision_score(y_valid, result)
     from sklearn.metrics import accuracy_score
     accuracy = accuracy_score(y_valid, result)
     print("Accuracy")
     print(accuracy)
-    #TODO: RETURN precision, and recall
     return (accuracy)
 
 
@@ -65,23 +73,33 @@ def pca(m):
 
 range_ay = np.arange(0.80, 1.0, 0.01)
 best_acc = 0
+best_m = 0
 print(
     "Feature extraction: Use PCA to reduce dimensionality to m, followed by k-NN. Try for different values of m corresponding to proportion of variance of 0.80, 0.81, 0.82, ...., 0.99")
-for m in list(range_ay):
-    train_X,valid_X = pca(m)
-    print("Reduced to m =" + '{:f}'.format(m))
-    acc = KNN(train_X,y_train,valid_X, y_valid)
-    if acc > best_acc:
-        best_acc = acc
-        best_m = m
+# for m in list(range_ay):
+#     train_X,valid_X = pca(m)
+#     print("Reduced to m =" + '{:f}'.format(m))
+#     acc = KNN(train_X,y_train,valid_X, y_valid)
+#     if acc > best_acc:
+#         best_acc = acc
+#         best_m = m
 
 print("Our best accuracy = " + "{:f}".format(best_acc))
 print("m was = " + "{:f}".format(best_m))
 
-#Plot the data for m=2.
-#TODO: PLOT DATA
+#%% plot the data for m=2.
+
 train_X, valid_X = pca(2)
 KNN(train_X, y_train, valid_X, y_valid)
+print(precision)
+print(recall)
+from mlxtend.plotting import plot_decision_regions
+import matplotlib.pyplot as plt
+plot_decision_regions(train_X, y_train, clf=knn, legend=2)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('PCA-Knn')
+plt.show()
 
 # Feature Selection: Use forward selection to reduce dimensionality to m using k-NN as predictor. Train the model for each m between 1 and 57.
 
@@ -95,36 +113,63 @@ feature_names = X.columns
 best_acc = 0
 print("# Feature Selection: Use forward selection to reduce dimensionality to m using k-NN as predictor.")
 X_train, X_valid, y_train, y_valid = train_test_split(X,y, test_size=0.5, random_state=42)
-for m in range(1,57):
+#PLEASE BE AWARE THAT CODE RUNS ON ALL AVAILABLE CPU (N_JOBS = -1)
+def sfs_knn(m):
+    global precision
+    global recall
+    global sfs1
     sfs1 = SFS(knn,
-               k_features=m,
-               forward=True,
-               floating=False,
-               scoring='accuracy',
-               n_jobs=-1)
+                   k_features=m,
+                   forward=True,
+                   floating=False,
+                   scoring='accuracy',
+                   n_jobs=-1)
     # now we can work with our selected features
     #.fit(X_train.iloc[:, feat_cols], y_train)
     print("m = " + "{:f}".format(m))
     pipe = Pipeline([('sfs1', sfs1),
-                          ('KNN', knn)])
+                              ('KNN', knn)])
     start = time.time()
     pipe.fit(X_train, y_train)
     stop = time.time()
     print(f"Training time: {stop - start}s")
     print(sfs1.k_feature_idx_)
     start = time.time()
+    cc = pipe.predict(X_valid)
     acc = pipe.score(X_valid, y_valid)
     stop = time.time()
     print(f"Valid time: {stop - start}s")
     print(acc)
-    if acc > best_acc:
-        best_acc = acc
-        feat_cols = pipe.named_steps['sfs1'].k_feature_names_
+    from sklearn.metrics import recall_score
+    recall = recall_score(y_valid, cc)
+    from sklearn.metrics import precision_score
+    precision = precision_score(y_valid, cc)
+    return acc
+# for m in range(1,57):
+#    acc = sfs_knn(m)
+#     if acc > best_acc:
+#         best_acc = acc
+#         feat_cols = pipe.named_steps['sfs1'].k_feature_names_
+#
+# print("Our best accuracy = " + "{:f}".format(best_acc))
+# print("Features selected by forward sequential selection was: "
+#       f"{feat_cols}")
 
-print("Our best accuracy = " + "{:f}".format(best_acc))
-print("Features selected by forward sequential selection was: "
-      f"{feat_cols}")
 
 
-
-
+#%% plot the data for m=2.
+X_train = X_train.values
+y_train = y_train.values
+X_valid = X_valid.values
+acc = sfs_knn(2)
+# print(precision)
+# print(recall)
+sfs1.fit(X_train, y_train)
+x_train_r = sfs1.transform(X_train)
+X_valid_r = sfs1.transform(X_valid)
+KNN(x_train_r,y_train,X_valid_r,y_valid)
+plot_decision_regions(x_train_r, y_train, clf=knn, legend=2)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Forward Selection KNN')
+plt.show()
